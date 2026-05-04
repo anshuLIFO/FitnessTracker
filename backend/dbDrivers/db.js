603bb40ae8a6ef859a13eae6aaccf3337ad53a36
Anshul Kamble <anshulkamble@Anshuls-MacBook-Air.local>
2026-05-03 17:05:13 -0700

    Update DB config for automatic Vercel Postgres integration

--- backend/dbDrivers/db.js
+++ backend/dbDrivers/db.js
@@ -1,13 +1,14 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Create a new pool using credentials from .env
+// Create a new pool using credentials from .env or Vercel
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.POSTGRES_URL || process.env.DATABASE_URL,
+  user: process.env.POSTGRES_USER || process.env.DB_USER,
+  host: process.env.POSTGRES_HOST || process.env.DB_HOST,
+  database: process.env.POSTGRES_DATABASE || process.env.DB_NAME,
+  password: process.env.POSTGRES_PASSWORD || process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
   ssl: { rejectUnauthorized: false }
 });
 
